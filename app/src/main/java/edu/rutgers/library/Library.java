b5935c17ffc4fe08104f68b5138e0aea95a603c7
Jared Tulayan <jared@jaredtulayan.xyz>
2021-02-03 23:31:58 -0500

    Add backend objects

--- app/src/main/java/edu/rutgers/library/Library.java
+++ app/src/main/java/edu/rutgers/library/Library.java
@@ -1,8 +1,67 @@
 package edu.rutgers.library;
 
-public class Library {
-    public Song[] songs; 
-    public Library() {
+import java.util.ArrayList;
+
+/**
+ * A collection of songs.
+ * <p>
+ * This class handles organization of songs ONLY. This class does NOT handle
+ * writing out the songs to a file, nor does this class handle displaying the
+ * list in a GUI (though it should print the song list as text just fine).
+ * <p>
+ * This is an extension of an {@code ArrayList}, so all those functions apply,
+ * as well as some niceties.
+ * 
+ * @see Song
+ */
+public class Library extends ArrayList<Song> {
+    /**
+     * Add a song to the collection, so long as it is not a duplicate.
+     * 
+     * @param s the song to add to the library
+     * @return  1 if successful,
+     *          0 otherwise
+     */
+    public boolean add(Song s) {
+       if (get(s.getName(), s.getArtist()) == null) {
+           super.add(s);
+           return true;
+       }
+
+       return false;
+    }
+
+    /**
+     * Finds a song by name and artist.
+     * 
+     * @param name   the name of the song to find
+     * @param artist the artist of the song to find
+     * @return       the {@code Song} with the same name and artist,
+     *               or {@code null} if nonexistent
+     */
+    public Song get(String name, String artist) {
+        for (Song s : this) {
+            if (s.getName().equals(name) && s.getArtist().equals(artist))
+                return s;
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets a string representing the full list of songs.
+     * <p>
+     * Really meant as a form of debug without GUI,
+     * or if you were to interface with this via CLI.
+     */
+    public String toString() {
+        // I use a StringBuilder to avoid constant re-allocation
+        // of a string object
+        StringBuilder b = new StringBuilder();
         
+        for (Song s : this)
+            b.append("" + s + System.lineSeparator());
+        
+        return b.toString();
     }
 }
--- app/src/main/java/edu/rutgers/library/Song.java
+++ app/src/main/java/edu/rutgers/library/Song.java
@@ -1,7 +1,12 @@
 package edu.rutgers.library;
 
 /**
+ * Representation of a long in the song library.
+ * <p> 
+ * This will maintain a way of storing a song's fields,
+ * as well as provide a standard of writing out a song's info. 
  * 
+ * @see Library
  */
 public class Song {
     private String name;
@@ -9,7 +14,81 @@ public class Song {
     private String artist;
     private int year;
 
+    /**
+     * Constructs a new Song with the provided fields.
+     * 
+     * @param n  the name of the song
+     * @param al the album the song is a part of
+     * @param ar the artist of the song
+     * @param y  the year the song was written
+     */
     public Song(String n, String al, String ar, int y) {
+        name = n;
+        album = al;
+        artist = ar;
 
+        if (y < 0) {
+            throw new IllegalArgumentException("Song year cannot be less than 0!");
+        }
+
+        year = y;
+    }
+
+    public void setName(String v) {
+        name = v;
+    }
+
+    public void setAlbum(String v) {
+        album = v;
+    }
+
+    public void setArtist(String v) {
+        artist = v;
+    }
+
+    public void setYear(int v) {
+        if (v < 0) {
+            throw new IllegalArgumentException("Song year cannot be less than 0!");
+        }
+
+        year = v;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * Displays the song as a string.
+     * 
+     * The format is as follows:
+     * <pre>
+     * "ARTIST - NAME [ALBUM, YEAR]"
+     * </pre>
+     * For instance, Hippo Campus' "Way it Goes" would display as
+     * <pre>
+     * "Hippo Campus - Way it Goes [Landmark, 2017]"
+     * </pre>
+     */
+    public String toString() {
+        return String.format(
+                    "%s - %s [%s, %s]", 
+                    artist,
+                    name,
+                    album,
+                    year == 0 ? "????" : "" + year
+                );
     }
 }
