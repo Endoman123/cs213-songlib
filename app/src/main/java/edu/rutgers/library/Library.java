fd8059a6310b82b0af919540ce8b81d35b10c5d7
Jared Tulayan <jared@jaredtulayan.xyz>
2021-02-03 22:23:01 -0500

    Edit .gitignore

---
+++ app/src/main/java/edu/rutgers/library/Library.java
@@ -0,0 +1,8 @@
+package edu.rutgers.library;
+
+public class Library {
+    public Song[] songs; 
+    public Library() {
+        
+    }
+}
---
+++ app/src/main/java/edu/rutgers/library/Song.java
@@ -0,0 +1,15 @@
+package edu.rutgers.library;
+
+/**
+ * 
+ */
+public class Song {
+    private String name;
+    private String album;
+    private String artist;
+    private int year;
+
+    public Song(String n, String al, String ar, int y) {
+
+    }
+}
